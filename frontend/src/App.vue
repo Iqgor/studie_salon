a0d995e27d18cdd4ae48d4a1fe51d5db21a9ab86
Generaalguan <33372@ma-web.nl>
2025-05-08 15:47:11 +0200

    kleuren switch werkt

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,7 @@ import appheader from './components/appHeader.vue'
 import SideWekkers from './components/SideWekkers.vue'
 import { onMounted, ref } from 'vue';
 import { auth } from '@/auth';
+import { sharedfunctions } from './sharedFunctions';
 
 export default {
   name: 'App',
@@ -19,41 +20,13 @@ export default {
   }
   ,
   methods: {
-    switchTheme(theme) {
-      document.documentElement.className = `theme-${theme}`;
-      localStorage.setItem('theme', theme);
-      this.currentTheme = theme;
-    },
 
-    checkTheme() {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      if (localStorage.getItem('theme') === 'dark') {
-        this.switchTheme('dark');
-        this.currentTheme = 'dark';
-      } else {
-        this.switchTheme('light');
-        this.currentTheme = 'light';
-      }
-    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-      if (localStorage.getItem('theme') === 'light') {
-        this.switchTheme('light');
-        this.currentTheme = 'light';
-      } else {
-        this.switchTheme('dark');
-        this.currentTheme = 'dark';
-      }
-    } else if (localStorage.getItem('theme') === 'dark') {
-      this.switchTheme('dark');
-      this.currentTheme = 'dark';
-    } else {
-      this.switchTheme('light');
-      this.currentTheme = 'light';
-    }
-    }
   },
   mounted() {
-    this.checkTheme();
     auth.check()
+    
+    const savedTheme = localStorage.getItem('theme') || 'light'
+    sharedfunctions.switchTheme(savedTheme)
   }
 }
 
