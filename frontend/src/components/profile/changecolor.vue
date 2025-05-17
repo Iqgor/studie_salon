259c329dcad2acc21e103ed819c37854975bc2f5
Generaalguan <33372@ma-web.nl>
2025-05-17 22:16:26 +0200

    je hebt nu een voorbeeld als je over de kleuren hovert

--- frontend/src/components/profile/changecolor.vue
+++ frontend/src/components/profile/changecolor.vue
@@ -1,23 +1,40 @@
 <template>
     <main>
-        <section class="top">
-            <!--hier komt een kaart met een element die van kleur veranderd zodra je over dat kleur hovert-->
+        <section class="demo">
+            <div class="demo__container" :style="{ backgroundColor: demoTheme.background }">
+                <div class="demo_card" :style="{ backgroundColor: demoTheme.card }">
+
+                    <div class="demo_sale" :style="{ backgroundColor: demoTheme.secondary }">
+                        <p class="demo_sale_p" :style="{ color: demoTheme.text }">100%</p>
+                    </div>
+
+                    <div class="demo_header" :style="{ color: demoTheme.text }">
+                        <span>
+                            <i class="fa-solid fa-palette" :style="{ color: demoTheme.secondary }"></i>
+                            <h2>demo voor de kleuren</h2>
+                        </span>
+                        <span class="demo_rank" :style="{ backgroundColor: demoTheme.secondary, color: demoTheme.text }">hey</span>
+                    </div>
+
+                    <p class="demo_description" :style="{ color: demoTheme.text }">hier wat uitleg</p>
+                    <p class="demo_price__container">
+                        <span class="demo_price" :style="{ color: demoTheme.text }"><i class="fa-solid fa-euro-sign" :style="{ color: demoTheme.text }"></i> $$$</span>
+                    </p>
+                    <hr :style="{ borderColor: demoTheme.text }">
+
+                    <button class="demo_btn" :style="{ backgroundColor: demoTheme.primary, color: demoTheme.text }">Neem Abonemment</button>
+                </div>
+            </div>
+
         </section>
-        <section class="bottom">
-            <article
-                class="card"
-                :class="{ 'card_active': theme.value === currentTheme }"
-                v-for="theme in themes"
-                :key="theme.value"
-                @click="handleClick(theme)"
-            >
+
+        <section class="cards">
+            <article class="card" :class="{ 'card_active': theme.value === currentTheme }" v-for="theme in themes"
+                :key="theme.value" @click="handleClick(theme)" @mouseenter="showDemo(theme)"
+                v-on:mouseleave="removeDemo()">
                 <i class="fa-solid fa-check card_icon" v-if="theme.value === currentTheme"></i>
-                <div
-                    class="card__stripe"
-                    v-for="(color, i) in getThemeStripeColors(theme)"
-                    :key="i"
-                    :style="{ backgroundColor: color }"
-                ></div>
+                <div class="card__stripe" v-for="(color, i) in getThemeStripeColors(theme)" :key="i"
+                    :style="{ backgroundColor: color }"></div>
             </article>
 
         </section>
@@ -37,6 +54,7 @@ export default {
     data() {
         return {
             themes: themes.themes,
+            demoTheme: {}
         };
     },
     computed: {
@@ -55,7 +73,7 @@ export default {
             sharedfunctions.switchTheme(theme.value, theme.name);
             this.setPageTheme(theme.value);
         },
-                getThemeStripeColors(theme) {
+        getThemeStripeColors(theme) {
             return [
                 theme.primary,
                 theme.secondary,
@@ -63,12 +81,146 @@ export default {
                 theme.background
             ];
         },
+        showDemo(theme) {
+            this.demoTheme = theme
+            console.log(this.demoTheme);
+        },
+        removeDemo() {
+            this.demoTheme = {}
+            console.log(this.demoTheme);
+        }
     }
 }
 </script>
 
 <style scoped>
-.bottom {
+
+hr {
+    border: 1px solid var(--color-text);
+    width: 100%;
+}
+main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+
+.demo {
+    width: 100%;
+    height: 100%;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0rem;
+}
+
+.demo__container{
+    padding: 3rem;
+    border-radius: 2rem;
+    border: black 0.3rem solid;
+    box-shadow:black 0.5rem 0.5rem 1rem  ;
+}
+
+.demo_card {
+    background-color: var(--color-card-500);
+    border-radius: 8px;
+    box-shadow: 0 0.2rem 0.4rem rgba(0, 0, 0, 0.1);
+    padding: 2rem;
+    min-width: 30rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    max-width: 30rem;
+    position: relative;
+}
+
+.demo_header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.demo_header span {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+.demo_header span h2 {
+    font-size: 1.5rem;
+    margin: 0;
+}
+
+.demo_header span i {
+    font-size: 2rem;
+    color: var(--color-secondary-500);
+}
+
+.demo_header .demo_rank {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: var(--color-background-100);
+    background-color: var(--color-secondary-500);
+    padding: 0.5rem 1rem;
+    border-radius: 4px;
+}
+
+.demo_btn {
+    background-color: var(--color-primary-500);
+    color: var(--color-background-100);
+    padding: 1rem 2rem;
+    border-radius: 0.4rem;
+    border: none;
+    cursor: pointer;
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-top: auto;
+}
+
+.demo_description {
+    font-size: 1.25rem;
+}
+
+.demo_price {
+    font-size: 2rem;
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 0.75rem;
+}
+
+.demo_price__container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+}
+
+.demo_sale{
+    position: absolute;
+    top: -1.5rem;
+    right: -1.5rem;
+    background: var(--color-secondary-500);
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+}
+
+.demo_sale_p{
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+
+
+
+
+
+.cards {
     display: flex;
     justify-content: center;
     align-items: center;
