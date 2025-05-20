0c8f01e528d8929a0845f16d3ba295ae2f8d6e7b
Kietmandjes <90683291+Kietmandjes@users.noreply.github.com>
2025-05-20 13:47:55 +0200

    update aan muziek

--- frontend/src/components/appHeader.vue
+++ frontend/src/components/appHeader.vue
@@ -6,6 +6,7 @@
         <img @click="$router.push('/')" src="/logo.png" alt="">
         <nav class="topMenuNav">
           <ul>
+            <li><a href="/">Home</a></li>
             <li><a href="/abonnementen">Abonnementen</a></li>
             <li><a href="#">Disclaimer</a></li>
             <li v-if="auth.isLoggedIn"><a href="#">Teksten</a></li>
@@ -70,7 +71,7 @@ export default {
     script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
     script.async = true;
     document.body.appendChild(script);
-    
+
   },
   methods: {
     changeColor() {
@@ -78,7 +79,7 @@ export default {
       document.getElementsByClassName('topMenu')[0].classList.toggle('topMenuClick')
       document.getElementsByClassName('icons')[0].classList.toggle('iconsClick')
     },
-    themeGradient(theme) {      
+    themeGradient(theme) {
       return {
         background: `linear-gradient(135deg, ${theme.background} 50%, ${theme.primary})`,
         color: theme.text,
