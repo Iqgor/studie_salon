623e20d914fcad6dd7ad237546a55ffb6d5503c1
Igor Herrebrugh <herrebrughigor@gmail.com>
2025-06-12 10:57:36 +0200

    Update add.php

--- backend/routes/carousel/add.php
+++ backend/routes/carousel/add.php
@@ -2,13 +2,6 @@
 $carouselName = $_POST['carouselName'] ?? null;
 $itemName = $_POST['itemName'] ?? null;
 
-$stmt = $conn->prepare("SELECT MAX(id) as last_id FROM coursel_items WHERE coursel_name = ?");
-$stmt->bind_param("s", $carouselName);
-$stmt->execute();
-$result = $stmt->get_result();
-$row = $result->fetch_assoc();
-$lastId = $row['last_id'] ?? 0;
-$newId = $lastId + 1;
 
 if (!$carouselName || !$itemName) {
     jsonResponse(['error' => 'Carousel name and item name are required'], 400);
@@ -17,10 +10,14 @@ if (!$carouselName || !$itemName) {
 
 $url = preg_replace('/[^\p{L}\p{N}\-]+/u', '', str_replace([' ', '/'], ['-', ''], $itemName));
 
-$stmt = $conn->prepare("INSERT INTO coursel_items (id,coursel_name, title, url) VALUES (?,?, ?, ?)");
-$stmt->bind_param("isss", $newId ,$carouselName, $itemName, $url);
+
+$stmt = $conn->prepare("INSERT INTO coursel_items (coursel_name,title, url) VALUES (?, ?, ?)");
+
+
+$stmt->bind_param("sss",$carouselName, $itemName, $url);
 if ($stmt->execute()) {
     jsonResponse(['success' => 'Item added successfully'], 200);
 } else {
     jsonResponse(['error' => 'Failed to add item'], 500);
 }
+?>
